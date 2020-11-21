0af4f784779828ab620b8ff21fc4540a9180a8f7
Taylor <63585450+Kordorgreyhoof@users.noreply.github.com>
2020-11-21 15:58:46 +0000

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -3,6 +3,7 @@ const dailyCanvas = document.getElementById('daily-chart');
 const mobileCanvas = document.getElementById("mobile-chart");
 const alertBanner = document.getElementById("alert");
 
+
 alertBanner.innerHTML = `
   <div class="alert-banner">
     <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete!</p>
